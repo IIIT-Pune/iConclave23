25944af0aaf2cc9640b23720ccaa310aad21e6ed
agarwalvinit5678 <agarwalvinit5678@gmail.com>
2023-10-01 18:30:17 +0530

    update

--- src/pages/Cult.jsx
+++ src/pages/Cult.jsx
@@ -1,42 +1,93 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-
+import "swiper/css/navigation"; // Import Swiper navigation styles
+import { useState, useEffect } from "react";
+import '../App.css'
 const Cult = () => {
+  const [swiper, setSwiper] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (swiper) {
+        const activeIndex = swiper.activeIndex;
+        const nextIndex =
+          activeIndex === swiper.slides.length - 1 ? 0 : activeIndex + 1;
+        swiper.slideTo(nextIndex);
+      }
+    }, 1500); // Change slide every 1.5 seconds
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [swiper]);
+
+  const goPrev = () => {
+    if (swiper) {
+      swiper.slidePrev();
+    }
+  };
+
+  const goNext = () => {
+    if (swiper) {
+      swiper.slideNext();
+    }
+  };
+
   return (
-    <div>
-      <div
-        style={{
-          background: "url(/images/tech_bg.png)",
-          backgroundPosition: "top center",
-          backgroundSize: "cover",
-        }}
-        className="h-screen"
-      >
-        <h1 className="text-white text-9xl p-14 font-octobercrow tracking-wider">
-          Cultural Events
-        </h1>
-        <div className="px-10 py-20">
-          <Swiper
-            slidesPerView={3}
-            spaceBetween={30}
-            centeredSlides={true}
-            pagination={{
-              clickable: true,
-            }}
-            className="mySwiper "
-          >
-            <SwiperSlide className="rounded-3xl">
-              <img
-                src="./images/Cultcard1.png"
-                alt=""
-                className="rounded-3xl bg-black"
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div>
+    <div
+      style={{
+        background: "url(/images/cult_back.png)",
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+        overflow: "hidden",
+      }}
+      className="tech-container h-screen"
+    >
+      {/* <h1 className="text-white text-9xl p-14 font-octobercrow tracking-wider">
+        Technical Events
+      </h1> */}
+      <img src="/images/cult_head.png" alt="Technical Events" className="mx-auto px-20" />
+      <div className="-mt-52 px-10 py-24">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          centeredSlides={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={{   // Enable navigation
+            prevEl: ".swiper-button-prev",
+            nextEl: ".swiper-button-next",
+          }}
+          className="mySwiper"
+          onSwiper={(swiper) => setSwiper(swiper)}
+        >
+          <SwiperSlide className="rounded-3xl">
+            <a href="/frameflick">
+              <img src="./images/frame_cau.png" alt="" className="rounded-3xl" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="rounded-3xl">
+            <a href="/cognition">
+              <img src="./images/cognition_cau.png" alt="" className="rounded-3xl" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="rounded-3xl">
+            <a href="/aawazeaawam">
+              <img src="./images/aawaaz_cau.png" alt="" className="rounded-3xl" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide className="rounded-3xl">
+            <a href="/lockout">
+              <img src="./images/lockout_cau.png" alt="" className="rounded-3xl" />
+            </a>
+          </SwiperSlide>
+        </Swiper>
+        {/* Previous Button */}
+        <div className="swiper-button-prev" onClick={goPrev}></div>
+        {/* Next Button */}
+        <div className="swiper-button-next" onClick={goNext}></div>
       </div>
     </div>
   );
--- src/pages/Tech.jsx
+++ src/pages/Tech.jsx
@@ -1,24 +1,54 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-
-
+import "swiper/css/navigation"; // Import Swiper navigation styles
+import { useState, useEffect } from "react";
+import '../App.css'
 const Tech = () => {
+  const [swiper, setSwiper] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (swiper) {
+        const activeIndex = swiper.activeIndex;
+        const nextIndex =
+          activeIndex === swiper.slides.length - 1 ? 0 : activeIndex + 1;
+        swiper.slideTo(nextIndex);
+      }
+    }, 1500); // Change slide every 1.5 seconds
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [swiper]);
+
+  const goPrev = () => {
+    if (swiper) {
+      swiper.slidePrev();
+    }
+  };
+
+  const goNext = () => {
+    if (swiper) {
+      swiper.slideNext();
+    }
+  };
+
   return (
     <div
       style={{
-        background: "url(/images/tech_bg.png)",
+        background: "url(/images/tech_2.png)",
         backgroundPosition: "top center",
         backgroundSize: "cover",
+        overflow: "hidden",
       }}
-      className="h-screen"
+      className="tech-container h-screen"
     >
-      <h1 className="text-white text-9xl p-14 font-octobercrow tracking-wider">
+      {/* <h1 className="text-white text-9xl p-14 font-octobercrow tracking-wider">
         Technical Events
-      </h1>
-      <div className="px-10 py-20">
+      </h1> */}
+      <img src="/images/tech_head.png" alt="Technical Events" className="mx-auto px-20" />
+      <div className="-mt-52 px-10 py-24">
         <Swiper
           slidesPerView={3}
           spaceBetween={30}
@@ -26,25 +56,38 @@ const Tech = () => {
           pagination={{
             clickable: true,
           }}
-          className="mySwiper "
+          navigation={{   // Enable navigation
+            prevEl: ".swiper-button-prev",
+            nextEl: ".swiper-button-next",
+          }}
+          className="mySwiper"
+          onSwiper={(swiper) => setSwiper(swiper)}
         >
           <SwiperSlide className="rounded-3xl">
-            <img src="./images/bg_off.png" alt="" className="rounded-3xl bg-black" />
+            <a href="/nexus">
+              <img src="./images/nexus_cau.png" alt="" className="rounded-3xl" />
+            </a>
           </SwiperSlide>
           <SwiperSlide className="rounded-3xl">
-            <img src="./images/lockout_bg.png" alt="" className="rounded-3xl" />
+            <a href="/infinity">
+              <img src="./images/infinity_cau.png" alt="" className="rounded-3xl" />
+            </a>
           </SwiperSlide>
           <SwiperSlide className="rounded-3xl">
-            <img src="./images/nexus_bg.png" alt="" className="rounded-3xl" />
+            <a href="/rofies1">
+              <img src="./images/offrd_cau.png" alt="" className="rounded-3xl" />
+            </a>
           </SwiperSlide>
           <SwiperSlide className="rounded-3xl">
-            <img
-              src="./images/infinity_bg.png"
-              alt=""
-              className="rounded-3xl"
-            />
+            <a href="/lockout">
+              <img src="./images/lockout_cau.png" alt="" className="rounded-3xl" />
+            </a>
           </SwiperSlide>
         </Swiper>
+        {/* Previous Button */}
+        <div className="swiper-button-prev" onClick={goPrev}></div>
+        {/* Next Button */}
+        <div className="swiper-button-next" onClick={goNext}></div>
       </div>
     </div>
   );
