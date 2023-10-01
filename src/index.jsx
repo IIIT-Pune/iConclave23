fc8b1be0057d78255af48b9f2545008d8d994dbe
agarwalvinit5678 <agarwalvinit5678@gmail.com>
2023-10-01 15:34:30 +0530

    scroll corrected

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
 import { Leva } from 'leva'
-
+import { useLocation } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 const portrait = window.innerHeight > window.innerWidth
 const portraitMultiplier = portrait ? 2 : 1
 
 const Home = () => {
+    const location = useLocation();
+    const isSpecificRoute = location.pathname === '/';
+    console.log(isSpecificRoute);
+    if (isSpecificRoute) {
+        // Get the root div and add CSS classes or styles
+        const rootDiv = document.getElementById('root');
+        if (rootDiv) {
+          // Add your CSS classes or styles here
+          rootDiv.classList.add('homeroot');
+        }
+      }
     return (
     <>
         <Canvas
