1f591eab4606ff035eafa10efa11bb18a6d6f66a
ashmit-coder <ashmitjagtap2209@gmail.com>
2023-10-16 14:47:08 +0530

    added links for HOF

--- src/assets/Halloffame.js
+++ src/assets/Halloffame.js
@@ -2,17 +2,20 @@ export const world = [
   {
     name: "Jay Leeds",
     clg: "Swarthmore College",
-    year: 22,
+    year: 22
   },
   {
     name: "Gennady Korotkevich",
     clg: "ITMO University",
     year: 21,
+    Linked:"https://www.linkedin.com/in/gennady-korotkevich-099316206/?originalSubdomain=ru",
+    GitHub:"https://github.com/mikyoch",
   },
   {
     name: "Jay Leeds",
     clg: "Swarthmore College",
     year: 20,
+
   },
   {
     name: "Kang",
@@ -23,6 +26,8 @@ export const world = [
     name: "Himanshu Jaju",
     clg: "Google",
     year: 18,
+    GitHub:"https://github.com/himanshujaju",
+    Linked:"https://www.linkedin.com/in/himanshujaju?originalSubdomain=uk"
   },
 ];
 export const india = [
@@ -30,6 +35,8 @@ export const india = [
     name: "Yash Daga",
     clg: "IIT Dhanbad",
     year: 22,
+    Linked:"https://www.linkedin.com/in/yash-daga-371218147/?originalSubdomain=in",
+
   },
   {
     name: "Kalash Gupta",
@@ -45,11 +52,14 @@ export const india = [
     name: "Shubham Atri",
     clg: "IIT Bombay",
     year: 19,
+    Linked:"https://www.linkedin.com/in/shubham-atri-2455a91a7/"
   },
   {
     name: "Priyanshu Kumar",
     clg: "IIIT Allahabad",
     year: 18,
+    Linked:"https://www.linkedin.com/in/priyanshukm/",
+    GitHub:"https://github.com/priyanshukm"
   },
 ];
 export const iiitp = [
@@ -57,25 +67,32 @@ export const iiitp = [
     name: "Gaurav",
     clg: "IIIT PUNE",
     year: 22,
+    Linked:"https://www.linkedin.com/in/g0uravlathwal/"
   },
   {
     name: "Apoorv Kumar",
     clg: "IIIT PUNE",
     year: 21,
+    Linked:"https://www.linkedin.com/in/apoorv-kumar-359555202/"
   },
   {
     name: "Gaurav",
     clg: "IIIT PUNE",
     year: 20,
+    Linked:"https://www.linkedin.com/in/g0uravlathwal/"
   },
   {
     name: "Harsh Prakash",
     clg: "IIIT PUNE",
     year: 19,
+    GitHub:"https://github.com/harsh132",
+    Linked:"https://www.linkedin.com/in/harsh132/"
   },
   {
     name: "Darshan Lokhande",
     clg: "IIIT PUNE",
     year: 18,
+    Linked:"https://www.linkedin.com/in/darshanlokhande/",
+    GitHub:"https://github.com/darshancool25"
   },
 ];
