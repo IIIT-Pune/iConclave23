2419bd59750388394b6d138eb54d10abe0142b4c
JokerSA <asatishubham2004@gmail.com>
2023-10-23 15:58:06 +0530

    coreteam

---
+++ src/assets/coremember.js
@@ -0,0 +1,9 @@
+export const CoreMember = [
+  {
+    name: "Shubham Asati",
+    designation:"Web Developer",
+    x:"",
+    insta:"",
+    linkedin:""
+  },
+];
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,5 +1,8 @@
 import LandingPageNav from "../components/LandingPageNav";
 import Footer from "../components/Footer";
+import Card from "../components/CoreTeamCards";
+import { CoreMember } from "../assets/coremember";
+
 const ContactUs = () => {
   return (
     <div
@@ -14,7 +17,20 @@ const ContactUs = () => {
       <h1 className="text-white text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-youmurdererbb text-center">
         Contact Us
       </h1>
-      
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-14 py-10 lg:py-16">
+        {CoreMember.map((member) => (
+          <Card
+            key={member.name}
+            name={member.name.toUpperCase()}
+            designation={member.designation.toUpperCase()}
+            X={member.github}
+            linkedin={member.linkedin}
+            insta={member.insta}
+          />
+        ))}
+      </div>
+
       <Footer textColor={"white"} />
     </div>
   );
