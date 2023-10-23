c296271bb44f9e54d68478d7b5f30aa7c7864d75
yashharne <yashharne.ed@gmail.com>
2023-10-23 13:35:32 +0530

    laptop about

--- src/App.jsx
+++ src/App.jsx
@@ -201,14 +201,25 @@ const App = () => {
               <img src={Loader} className="w-[60%]" />
             </div>
           )}
-          {isLargeScreen ? <About /> : <AboutMobile />}
+          <AboutMobile />
+          {/* {1 == 0 ? <About /> : <AboutMobile />} */}
         </>
         // <LandingAbout />
       ),
     },
     {
       path: "cultnight",
-      element: <CultNight />,
+      element: (
+        <>
+          {" "}
+          {showLoader2 && (
+            <div className="h-screen fixed w-full z-[999999] overflow-hidden flex items-center justify-center bg-black ">
+              <img src={Loader} className="w-[60%]" />
+            </div>
+          )}
+          <CultNight />
+        </>
+      ),
     },
     {
       path: "sponsors",
@@ -220,7 +231,8 @@ const App = () => {
               <img src={Loader} className="w-[60%]" />
             </div>
           )}
-          {1 == 0 ? <Sponsor /> : <SponsorsMobile />}
+          <SponsorsMobile />
+          {/* {1 == 0 ? <Sponsor /> : <SponsorsMobile />} */}
         </>
       ),
     },
