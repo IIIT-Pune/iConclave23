25a18b990ee7bc54485ef0edf87f622f3882b90a
yashharne <yashharne.ed@gmail.com>
2023-10-24 16:00:37 +0530

    landing button changes

--- src/components/EventButton.jsx
+++ src/components/EventButton.jsx
@@ -1,28 +1,26 @@
-const EventButton = ({type}) => {
+const EventButton = ({ type }) => {
   return (
     <div>
-      <a href="#">
-        <div className="relative">
-          <img
-            src="/images/b1img.png"
-            alt=""
-            className={`w-10 absolute -top-5 -right-7`}
-          />
-          <img
-            src="/images/b2img.png"
-            alt=""
-            className="w-10 absolute -top-5 -left-7  z-0"
-          />
-          <h1
-            className="text-center"
-            style={{
-              textShadow: "0 0 20px rgba(255, 255, 255, 0.7)"
-            }}
-          >
-            {type} EVENTS
-          </h1>
-        </div>
-      </a>
+      <div className="relative">
+        <img
+          src="/images/b1img.png"
+          alt=""
+          className={`w-10 absolute -top-5 -right-7`}
+        />
+        <img
+          src="/images/b2img.png"
+          alt=""
+          className="w-10 absolute -top-5 -left-7  z-0"
+        />
+        <h1
+          className="text-center"
+          style={{
+            textShadow: "0 0 20px rgba(255, 255, 255, 0.7)",
+          }}
+        >
+          {type} EVENTS
+        </h1>
+      </div>
     </div>
   );
 };
